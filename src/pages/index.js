a47651be36b4d2aa1d0f37a1f1a785626d404c2d
David Tsai <david@matternet.us>
2018-11-10 14:27:34 -0800

    Adding ant design button with styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { Button } from 'antd'
 
 import Layout from '../components/layout'
 import Image from '../components/image'
@@ -11,6 +12,7 @@ const IndexPage = () => (
     <p>Now go build something great.</p>
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
+      <Button type="primary">Ant Design Button!</Button>
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
